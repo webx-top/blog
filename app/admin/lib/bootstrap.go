8be5a370f01714b875ccc03779916cb15a8cf725
swh <swhbox@foxmail.com>
2016-01-12 14:15:54 +0800

    update

--- app/admin/lib/bootstrap.go
+++ app/admin/lib/bootstrap.go
@@ -1,15 +1,38 @@
 package lib
 
 import (
+	"html/template"
+
 	X "bitbucket.org/admpub/webx"
 	"bitbucket.org/admpub/webx/lib/tplex"
 	"github.com/webx-top/blog/app/base"
+	"github.com/webx-top/blog/app/base/lib/tplfunc"
 )
 
-var App = X.Serv().NewApp("admin", base.Language.Store(), base.SessionMW)
+var (
+	Name       = `admin`
+	App        = X.Serv().NewApp(Name, base.Language.Store(), base.SessionMW)
+	FuncMap    = tplfunc.TplFuncMap
+	StaticPath = `/assets`
+	Static     = tplfunc.NewStatic(`/` + Name + StaticPath)
+)
 
 func init() {
-	te := tplex.New(base.ThemePath(`admin`))
+	tp := base.ThemePath(`admin`)
+	te := tplex.New(tp)
 	te.InitMgr(true, true)
-	App.Webx().SetRenderer(te)
+
+	FuncMap["StaticUrl"] = Static.StaticUrl
+	FuncMap["JsUrl"] = Static.JsUrl
+	FuncMap["CssUrl"] = Static.CssUrl
+	FuncMap["ImgUrl"] = Static.ImgUrl
+	FuncMap["JsTag"] = Static.JsTag
+	FuncMap["CssTag"] = Static.CssTag
+	FuncMap["ImgTag"] = Static.ImgTag
+	te.FuncMapFn = func() template.FuncMap {
+		return FuncMap
+	}
+	x := App.Webx()
+	x.SetRenderer(te)
+	x.Static(StaticPath, tp+StaticPath)
 }
