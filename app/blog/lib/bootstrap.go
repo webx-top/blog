05d20a62aeb7c00a078d1003a89cca36a6a5f3a3
swh <swhbox@foxmail.com>
2016-01-28 13:11:52 +0800

    added ping

--- app/blog/lib/bootstrap.go
+++ app/blog/lib/bootstrap.go
@@ -2,6 +2,13 @@ package lib
 
 import (
 	"github.com/webx-top/blog/app/base"
+	X "github.com/webx-top/webx"
 )
 
 var App = base.Server.NewApp("", base.Language.Store(), base.SessionMW, base.HtmlCacheMW)
+
+func init() {
+	App.R(`/ping`, func(c *X.Context) error {
+		return c.String(200, `pong`)
+	})
+}
