5d24c693ebc5ad791ecf6426697cf877c32cf0d8
Wenhui Shen <swhbox@foxmail.com>
2016-02-07 15:25:38 +0800

    update

--- data/theme/admin/assets/js/footer.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  	webx.initPage();
-	if ($(window).width()>1025 && $('div.contents').length && $('.contents_wrapper').length && $('aside.sidebar').length) {
-		$('div.contents').css('width',$('.contents_wrapper').width()-$('aside.sidebar').width());
-	}
-});
