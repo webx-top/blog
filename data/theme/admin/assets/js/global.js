2aabd6f3c50fa25569b57b03c7f1fe2e7abcb143
swh <swhbox@foxmail.com>
2016-03-24 16:14:17 +0800

    add js date function

--- data/theme/admin/assets/js/global.js
+++ data/theme/admin/assets/js/global.js
@@ -1035,3 +1035,50 @@ function XHR(url,param,fn,type,method){
         alert(resp);
     },type);
 }
+
+/**
+ * 字符串转时间戳
+ * @param dateString 格式化后的时间 (如：2016-09-01 09:30:00)
+ */
+function strToTime(dateString){
+    var date = new Date(dateString),timestamp = date.getTime();
+    if(!isNaN(timestamp))return timestamp;
+    var s = dateString.replace(/[^\d-]+/g,'-'),arr = s.split('-');
+    for (var i = 5; i >= 0; i--) {
+        if(typeof(arr[i])=='undefined'){
+            arr[i]=i>0&&i<3?1:0;
+            continue;
+        }
+        break;
+    };
+    date = new Date(Date.UTC(arr[0],arr[1]-1,arr[2],arr[3]-8,arr[4],arr[5]));
+    timestamp = date.getTime();
+    return timestamp;
+}
+
+/**
+ * 友好时间
+ * @param sTime 开始时间
+ * @param cTime 当前时间
+ * @return {string}
+ */
+function friendlyDate(sTime, cTime) {
+    var formatTime = function (num) {
+        return (num < 10) ? '0' + num : num;
+    };
+    if (!sTime) return '';
+    var cDate = new Date(cTime * 1000);
+    var sDate = new Date(sTime * 1000);
+    var dTime = cTime - sTime;
+    var dDay = parseInt(cDate.getDate()) - parseInt(sDate.getDate());
+    var dMonth = parseInt(cDate.getMonth() + 1) - parseInt(sDate.getMonth() + 1);
+    var dYear = parseInt(cDate.getFullYear()) - parseInt(sDate.getFullYear());
+    if (dTime < 60) {
+        if (dTime < 10) return '刚刚';
+        return parseInt(Math.floor(dTime / 10) * 10) + '秒前';
+    } 
+    if (dTime < 3600) return parseInt(Math.floor(dTime / 60)) + '分钟前';
+    if (dYear === 0 && dMonth === 0 && dDay === 0) return '今天' + formatTime(sDate.getHours()) + ':' + formatTime(sDate.getMinutes());
+    if (dYear === 0) return formatTime(sDate.getMonth() + 1) + '月' + formatTime(sDate.getDate()) + '日 ' + formatTime(sDate.getHours()) + ':' + formatTime(sDate.getMinutes());
+    return sDate.getFullYear() + '-' + formatTime(sDate.getMonth() + 1) + '-' + formatTime(sDate.getDate()) + ' ' + formatTime(sDate.getHours()) + ':' + formatTime(sDate.getMinutes());
+}
