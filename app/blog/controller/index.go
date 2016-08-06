e9391df3ac21f3794b1acdd3ecba12dd7281a0f4
Wenhui Shen <swhbox@foxmail.com>
2016-08-06 11:12:36 +0800

    update

--- app/blog/controller/index.go
+++ app/blog/controller/index.go
@@ -29,6 +29,7 @@ import (
 	"github.com/webx-top/webx/lib/com"
 	fileStore "github.com/webx-top/webx/lib/store/file"
 
+	"github.com/admpub/log"
 	D "github.com/webx-top/blog/app/base/dbschema"
 )
 
@@ -75,30 +76,32 @@ func (a *Index) Index() error {
 		"test": "times---",
 		"r":    []string{"one", "two", "three"},
 	})
+	log.Sync()
+	defer log.Sync(false)
 	m := &D.Post{}
 	a.DB.Id(1).Get(m)
 	ms := []*PostCollection{}
-	a.Logger.Debug(`测试extends查询：`)
+	log.Info(`测试extends查询：`)
 	a.DB.Where(`Post.id=1`).Join(`LEFT`, `webx_user`, `User.id=Post.uid`).
 		Join(`LEFT`, `webx_ocontent`, `Ocontent.rc_id=Post.id AND Ocontent.rc_type=?`, `post`).Find(&ms)
 	ms2 := []*Post2{}
 
-	a.Logger.Debug(`测试extends带普通字段的查询：`)
+	log.Info(`测试extends带普通字段的查询：`)
 	a.DB.Where(`Post.id=1`).Join(`LEFT`, `webx_user`, `User.id=Post.uid`).
 		Join(`LEFT`, `webx_ocontent`, `Ocontent.rc_id=Post.id AND Ocontent.rc_type=?`, `post`).Find(&ms2)
 
-	a.Logger.Debug(`测试忽略Post.content字段的查询：`)
+	log.Info(`测试忽略Post.content字段的查询：`)
 	//自动加表前缀
 	a.DB.Omit(`Post.content`).Where(`Post.id=1`).Join(`LEFT`, `~user`, `User.id=Post.uid`).
 		Join(`LEFT`, `~ocontent`, `Ocontent.rc_id=Post.id AND Ocontent.rc_type=?`, `post`).Find(&ms2)
 
-	a.Logger.Debug(`测试通过rel标签指定关联条件的查询：`)
+	log.Info(`测试通过rel标签指定关联条件的查询：`)
 	//使用rel标签
 	ms3 := []*PostCollection2{}
 	a.DB.Omit(`User.passwd`).Where(`Post.id=2`).Find(&ms3)
 	com.Dump(ms3)
 
-	a.Logger.Debug(`测试通过alias标签指定表别名的查询：`)
+	log.Info(`测试通过alias标签指定表别名的查询：`)
 	msAlias := []*Post3{}
 	a.DB.Where(`a.id=1`).Join(`LEFT`, `webx_user`, `a.id=b.uid`).
 		Join(`LEFT`, `webx_ocontent`, `c.rc_id=a.id AND c.rc_type=?`, `post`).Find(&msAlias)
